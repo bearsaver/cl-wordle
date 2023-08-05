ae51f423f5aa9b4b8e4bb9c5fbeb1fda8a862abc
Kate Friedman <kate.friedman.2006@gmail.com>
2023-08-05 17:10:34 -0400

    finished

--- wordle.py
+++ wordle.py
@@ -1,6 +1,9 @@
 # green = 1, yellow = 0, gray = -1
 
 from colorama import Fore, Back, Style
+from random import randrange
+
+words = ['hello', 'world', 'spade', 'joker', 'spree', 'trace', 'scold']
 
 def green(char):
 	print(Fore.GREEN + char, end="")
@@ -14,46 +17,60 @@ def white(char):
 def reset():
 	print(Style.RESET_ALL)
 
-
-
 def check_guess(guess, word):
 	char_cts = get_word_info(word)
-	print(char_cts)
-	ret = 0
+	res = [0 for i in range(0, 5)]
 	for i in range(0, len(guess)):
 		if guess[i] in word:
 			if guess[i] == word[i]:
-				if char_cts[word[i]] != 0:
-					green(guess[i])
-					char_cts[word[i]] -= 1
-					ret += 1
+				if char_cts[guess[i]] > 0:
+					res[i] = 1
+					char_cts[guess[i]] -= 1
+	
+	for i in range(0, len(guess)):
+		if guess[i] in word:
+			if guess[i] != word[i]:
+				if char_cts[guess[i]] > 0:
+					res[i] = 0
+					char_cts[guess[i]] -= 1
 				else:
-					white(guess[i])
-			else:
-				if char_cts[word[i]] != 0:
-					yellow(guess[i])
-					char_cts[word[i]] -= 1
-				else:
-					white(guess[i])
-		else:
+					res[i] = -1
+	
+	for i in range(0, len(guess)):
+		if guess[i] not in word:
+			res[i] = -1
+	
+	return res
+
+def print_res(res, guess):
+	for i in range(0, len(res)):
+		if res[i] == 1:
+			green(guess[i])
+		elif res[i] == 0:
+			yellow(guess[i])
+		elif res[i] == -1:
 			white(guess[i])
 	reset()
-	
-	return ret
+
+def check_for_solve(res, guess):
+	if res == [1 for i in range(0, 5)]:
+		return 1
+	else:
+		return 0
 
 def get_word_info(word):
 	chars = []
-	ret = {}
+	res = {}
 	for i in range(0, len(word)):
 		if not (word[i] in chars):
 			chars.append(word[i])
-			ret[word[i]] = 1
+			res[word[i]] = 1
 		else:
-			ret[word[i]] += 1
-	return ret
+			res[word[i]] += 1
+	return res
 
 # pick words, create list of characters
-answer = "hello"
+word = words[randrange(0, len(words) - 1)]
 
 guess_number = 0
 
@@ -63,7 +80,7 @@ while(guess_number < 6):
 
 	# validate user input - make sure it's alphabetic and 5 chars
 	while(True):
-		guess = input(f"guess {guess_number}: ")
+		guess = input(f"guess {guess_number + 1}: ")
 		if (len(guess) == 5) and (guess.isalpha() == True):
 			guesses.append(guess)
 			guess_number += 1
@@ -74,8 +91,10 @@ while(guess_number < 6):
 	# check guess
 	guess = guesses[guess_number - 1]
 
+	res = check_guess(guess, word)
+	
+	print_res(res, guess)
 
-
-	if check_guess(guess, answer) == 5:
+	if check_for_solve(res, guess):
 		print('You got the word!')
 		break
